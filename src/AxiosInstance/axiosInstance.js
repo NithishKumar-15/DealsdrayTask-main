49e923eece469357b734844007471ce38a7ee994
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-10-19 12:52:28 +0530

    axios insrance api changed

--- src/AxiosInstance/axiosInstance.js
+++ src/AxiosInstance/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:`${import.meta.env.BASEURL}`
-    baseURL: "http://localhost:4000"
+    // baseURL: "http://localhost:4000"
+    baseURL:"https://dealsdraytask-be.onrender.com"
 })
 
 export default instance
